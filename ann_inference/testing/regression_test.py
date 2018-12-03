1e21e57aefccc76b7ac649d631da40d587166c3a
Brett Patterson <bpatter5@MacBook-Pro.fios-router.home>
2018-12-03 05:27:51 -0500

    12/3/18 modeling adds

--- ann_inference/testing/regression_test.py
+++ ann_inference/testing/regression_test.py
@@ -8,7 +8,7 @@
 
 import numpy as np
 import pickle 
-from ann_inference.regression.feedforward import RegressionFF as rff
+from ann_inference.regression import feedforward  as rff
 import torch
 from torch.nn import functional as F
 
@@ -17,18 +17,18 @@ from torch.nn import functional as F
 # outputs a pickled class object
 def save_test(reg_test, file):
     # open a file in write binary mode
-    with open(file, 'wb'):
+    with open(file, 'wb') as f:
         # pickle object to file
-        pickle.dump(reg_test, file)
+        pickle.dump(reg_test, f)
 
 # method to load pickled file
 # takes a file path as input
 # returns a pickled object into memory
 def load_test(file):
     # open a pickled file in binary mode
-    with open(file, 'rb'):
+    with open(file, 'rb') as f:
         # return the pickled object
-        return(pickle.load(file))
+        return(pickle.load(f))
     
 # class to run testing on FF networks
 # class takes a state dict and model data as input
@@ -37,7 +37,7 @@ class RegressionTester():
     # ann_inference.data.load_data.ModelData as input
     def __init__(self, model_data, hidden_units, opt = torch.optim.SGD, loss_func = F.mse_loss):
         self.model_data = model_data
-        self.model = rff(input_size = self.model_data.get_X_train().shape[1], hidden_units=hidden_units)
+        self.model = rff.RegressionFF(input_size = self.model_data.X.shape[1], hidden_units=hidden_units)
         self.opt = opt(self.model.parameters(), 1e-5)
         self.loss_func = loss_func
         
@@ -45,9 +45,15 @@ class RegressionTester():
     def fit(self, num_epochs, num_batch):
         # zero grads 
         self.opt.zero_grad()
+        
         # generate torch tensors from numpy arrays in model_data
         X = torch.Tensor(self.model_data.X)
         y = torch.Tensor(np.reshape(self.model_data.y, [self.model_data.y.shape[0] , 1]))
+        
+        # storing mse for each epoch 
+        mse_epoch = np.zeros(num_epochs)
+        weight_1 = np.zeros([num_epochs, self.model.linear_1.weight.shape[0]])
+        weight_2 = np.zeros([num_epochs, self.model.linear_2.weight.shape[0]])
         
         # iterate over samples for num_empochs
         for epoch in range(num_epochs):
@@ -71,4 +77,12 @@ class RegressionTester():
                 # shuffle the training indices so the batches
                 # are different each epoch
                 self.model_data.shuffle_train_idx()
- 
+            
+            # predicted loss compared to actual at epoch
+            epoch_loss = self.model.forward(X[self.model_data.train_idx,:])
+            mse_epoch[epoch] = self.loss_func(epoch_loss, y[self.model_data.train_idx,:])
+            # storing the mean weight for each epoch along the columns
+            weight_1[epoch,:] = np.mean(self.model.linear_1.weight.detach().numpy(), axis=1)
+            weight_2[epoch,:] = np.mean(self.model.linear_2.weight.detach().numpy(), axis=1)
+            
+        return((mse_epoch, weight_1, weight_2))
