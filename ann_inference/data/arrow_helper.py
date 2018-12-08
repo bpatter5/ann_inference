d0ac8b722187c40178ea2ad37cb98572f8926cb6
Brett Patterson <bpatter5@MacBook-Pro.fios-router.home>
2018-12-08 07:22:55 -0500

    12/8/18 PyArrow adds

---
+++ ann_inference/data/arrow_helper.py
@@ -0,0 +1,132 @@
+# =============================================================================
+# Created By: bpatter5
+# Updated By: bpatter5
+# Created On: 12/3/2018
+# Updated On: 12/8/2018
+# Purpose: 
+# =============================================================================
+
+import pyarrow as pa
+from pyarrow import parquet as pq
+import numpy as np
+
+
+
+def write_test(path, test, context):
+    '''
+    Description
+    -----------
+    Method to serialize a test and write it to disk.
+    
+    Parameters
+    ----------
+    path : string
+        File path for which to write the serialized object to.
+    
+    test : ann_inference.testing.xxxx_test
+        Test to serialize and write to disk.
+    
+    context : pa.SerializationContext
+        Custom serialization context that defines how the test is to be written
+    
+    Returns
+    -------
+        : void 
+        Writes a serialized pyarrow buffer to disk        
+    
+    '''
+    
+    buf = context.serialize(test).to_buffer()
+    
+    with open(path, 'wb') as f:
+        f.write(buf)
+        
+def read_test(path, context):
+    '''
+    Description
+    -----------
+    Read a serialized pyarrow buffer from disk and return a test object.
+    
+    Parameters
+    ----------
+    path : string
+        File path to serialized test
+    
+    context: pa.SerializationContext
+        Custom serialization context to read the test back into memory
+        
+    Returns
+    -------
+        : ann_inference.testing.xxxx_test
+        Desearialized test using the given context
+    '''
+    mmap = pa.memory_map(path)
+    
+    return(context.deserialize(mmap.read_buffer()))
+
+
+def gen_parquet_batch(test_stat, fill_col, epoch_num, test_num, col_names):
+    '''
+    Description
+    -----------
+    Generate a pyarrow.RecordBatch from a numpy array of PyTorch model parameters.
+    
+    Parameters
+    ----------
+    test_stat : np.array
+       Array of parameters to be written to batch and eventually saved to disk for later testing
+    
+    fill_col : string
+        Name of the parameter being tested which will later become a partition on disk
+    
+    epoch_num : int
+        Current epoch
+    
+    test_num : int
+        Current test assuming multiple model runs
+        
+    col_names : list[string]
+        Column names to assign to fields in the current batch
+    
+    Returns
+    -------
+        : pyarrow.RecordBatch
+        Record batch with nrows(test_stat) x ncols(test_stat) + 3
+        
+    '''
+    data = []
+    
+    data.append(pa.array(np.full(test_stat.shape[0], fill_col)))
+    data.append(pa.array(np.full(test_stat.shape[0], test_num)))
+    data.append(pa.array(np.full(test_stat.shape[0], epoch_num)))
+      
+    for j in np.arange(0, test_stat.shape[1]):
+        data.append(pa.array(test_stat[:,j]))
+    
+
+    return(pa.RecordBatch.from_arrays(data, col_names))
+
+def results_to_parquet(path, batch_list):
+    '''
+    Description
+    -----------
+    Function turns a list of batches into a parquet dataset on disk for future analysis.
+    
+    Parameters
+    ----------
+    path : string
+        Base path to create parquet dataset at
+    
+    batch_list : list[pyarrow.RecordBatch]
+        Batches with matching schema definitions to be written to disk
+    
+    Returns
+    -------
+        : void
+        Writes a parquet dataset to disk
+    
+    '''
+    
+    tbl = pa.Table.from_batches(batch_list)
+    
+    pq.write_to_dataset(tbl, path, partition_cols=['stat', 'id']) 
--- ann_inference/testing/regression_test.py
+++ ann_inference/testing/regression_test.py
@@ -7,42 +7,50 @@
 # =============================================================================
 
 import numpy as np
-import pickle 
+import pyarrow as pa
+from ann_inference.data import load_data as ld
 from ann_inference.regression import feedforward  as rff
+from ann_inference.data import arrow_helper as ah
 import torch
 from torch.nn import functional as F
+import pandas as pd
+from pyarrow import parquet as pq
 
-# method to pickle test class
-# takes an object and file as input
-# outputs a pickled class object
-def save_test(reg_test, file):
-    # open a file in write binary mode
-    with open(file, 'wb') as f:
-        # pickle object to file
-        pickle.dump(reg_test, f)
 
-# method to load pickled file
-# takes a file path as input
-# returns a pickled object into memory
-def load_test(file):
-    # open a pickled file in binary mode
-    with open(file, 'rb') as f:
-        # return the pickled object
-        return(pickle.load(f))
-    
 # class to run testing on FF networks
 # class takes a state dict and model data as input
 class RegressionTester():
-    # method takes a PyTorch state_dict and 
-    # ann_inference.data.load_data.ModelData as input
+    '''
+    Description
+    -----------
+    Class to run testing on PyTorch models. Relies heavily on the Apache Arrow project 
+    and the Parquet format to efficiently write model parameters to disk following each run of fit to avoid 
+    overloading in-memory operations. The Parquet format allows for the files on disk to be read and analyzed by a variety of tools
+    {Spark, Drill, Pandas, etc.}
+
+    Parameters
+    ----------
+    model_data : ann_inference.data.load_data.ModelData
+        Test data to input into model. Using synthetic so all aspects of the dataset are
+        known and controlled to make for easy testing across a variety of parameters.
+    
+    hidden_units : int
+        Desired number of hidden units per hidden layer in the PyTorch model
+    
+    opt : torch.optim.xxxx, torch.optim.SGD
+    
+    loss_func : torch.nn.Functional.xxxx, torch.nn.Functional.mse_loss
+    
+    '''
     def __init__(self, model_data, hidden_units, opt = torch.optim.SGD, loss_func = F.mse_loss):
         self.model_data = model_data
+        self.hidden_units = hidden_units
         self.model = rff.RegressionFF(input_size = self.model_data.X.shape[1], hidden_units=hidden_units)
         self.opt = opt(self.model.parameters(), 1e-5)
         self.loss_func = loss_func
         
     # method to fit model to dataset 
-    def fit(self, num_epochs, num_batch):
+    def fit(self, num_epochs, num_batch, test_id, path):
         # zero grads 
         self.opt.zero_grad()
         
@@ -50,10 +58,29 @@ class RegressionTester():
         X = torch.Tensor(self.model_data.X)
         y = torch.Tensor(np.reshape(self.model_data.y, [self.model_data.y.shape[0] , 1]))
         
+        
         # storing mse for each epoch 
-        mse_epoch = np.zeros(num_epochs)
-        weight_1 = np.zeros([num_epochs, self.model.linear_1.weight.shape[0]])
-        weight_2 = np.zeros([num_epochs, self.model.linear_2.weight.shape[0]])
+        batch_dict = {'mse':list(), 'weight_1':list(), 'weight_2':list()}
+        
+        weight_cols = {'mse':['test_stat', 'test_num', 'epoch_num', 'mse']}
+        
+        weight_cols['weight_1'] = []
+        weight_cols['weight_1'].append('test_stat')
+        weight_cols['weight_1'].append('test_num')
+        weight_cols['weight_1'].append('epoch_num')
+        
+        weight_cols['weight_1'].extend(['weight_' + str(i) for i in np.arange(0, self.model.linear_1.weight.shape[0])])
+        
+        weight_cols['weight_2'] = []
+        weight_cols['weight_2'].append('test_stat')
+        weight_cols['weight_2'].append('test_num')
+        weight_cols['weight_2'].append('epoch_num')
+        
+        weight_cols['weight_2'].extend(['weight_' + str(i) for i in np.arange(0, self.model.linear_2.weight.shape[0])])
+        
+        #mse_epoch = np.zeros(num_epochs)
+        #weight_1 = np.zeros([num_epochs, self.model.linear_1.weight.shape[0]])
+        #weight_2 = np.zeros([num_epochs, self.model.linear_2.weight.shape[0]])
         
         # iterate over samples for num_empochs
         for epoch in range(num_epochs):
@@ -79,10 +106,37 @@ class RegressionTester():
                 self.model_data.shuffle_train_idx()
             
             # predicted loss compared to actual at epoch
-            epoch_loss = self.model.forward(X[self.model_data.train_idx,:])
-            mse_epoch[epoch] = self.loss_func(epoch_loss, y[self.model_data.train_idx,:])
-            # storing the mean weight for each epoch along the columns
-            weight_1[epoch,:] = np.mean(self.model.linear_1.weight.detach().numpy(), axis=1)
-            weight_2[epoch,:] = np.mean(self.model.linear_2.weight.detach().numpy(), axis=1)
+            epoch_loss = self.model.forward(X[self.model_data.test_idx,:])
+            batch_dict['mse'].append(ah.gen_parquet_batch(self.loss_func(epoch_loss, y[self.model_data.test_idx,:]), fill_col='mse',
+                      epoch_num=epoch, test_num=test_id, col_names=weight_cols['mse']))
             
-        return((mse_epoch, weight_1, weight_2))
+            # storing the mean weight for each epoch along the columns
+            batch_dict['weight_1'].append(ah.gen_parquet_batch(np.mean(self.model.linear_1.weight.detach().numpy(), axis=1) , 
+                      fill_col='weight_1', epoch_num=epoch, test_num=test_id, col_names=weight_cols['weight_1']))
+            batch_dict['weight_2'].append(ah.gen_parquet_batch(np.mean(self.model.linear_2.weight.detach().numpy(), axis=1) ,
+                      epoch_num=epoch, test_num=test_id, col_names=weight_cols['weight_2']))
+        
+        for key, batches in batch_dict.items():
+            ah.results_to_parquet(path=path, batch_list=batches)
+        #return((mse_epoch, weight_1, weight_2))
+        
+        
+        def gen_test_datasets(self, num_tests, num_epochs, num_batch):
+            for i in np.arange(0, num_tests):
+                self.model_vitals[i] = self.fit(num_epochs=num_epochs, num_batch=num_batch)
+
+
+
+def _serialize_RegressionTester(test):
+    return({'X':test.model_data.X, 'y':test.model_data.y, 'seed':test.model_data.seed,
+            'train_pct':test.model_data.train_pct, 'hidden_units':test.hidden_units})
+
+def _deserialize_RegressionTester(test):
+    return(RegressionTester(ld.ModelData(test['X'], test['y'], test['seed'],
+                                          test['train_pct']), test['hidden_units']))  
+    
+        
+context = pa.SerializationContext()
+context.register_type(RegressionTester, 'RegressionTester' , 
+                      custom_serializer=_serialize_RegressionTester ,
+                      custom_deserializer=_deserialize_RegressionTester)
